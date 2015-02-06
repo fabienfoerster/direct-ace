8278412b3de87a9f2d74bd1a04287c0260e87eb1
clement0210 <duffau@polytech.unice.fr>
2015-02-06 17:56:31 +0100

    Bugfix on push servlet

--- back-end/dataaccesor/src/main/java/fr/unice/polytech/directace/acessor/MatchLog.java
+++ back-end/dataaccesor/src/main/java/fr/unice/polytech/directace/acessor/MatchLog.java
@@ -68,4 +68,32 @@ public class MatchLog {
                 ", matchID='" + matchID + '\'' +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MatchLog matchLog = (MatchLog) o;
+
+        if (date != null ? !date.equals(matchLog.date) : matchLog.date != null) return false;
+        if (eventName != null ? !eventName.equals(matchLog.eventName) : matchLog.eventName != null) return false;
+        if (id != null ? !id.equals(matchLog.id) : matchLog.id != null) return false;
+        if (matchID != null ? !matchID.equals(matchLog.matchID) : matchLog.matchID != null) return false;
+        if (playerID != null ? !playerID.equals(matchLog.playerID) : matchLog.playerID != null) return false;
+        if (value != null ? !value.equals(matchLog.value) : matchLog.value != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (eventName != null ? eventName.hashCode() : 0);
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        result = 31 * result + (playerID != null ? playerID.hashCode() : 0);
+        result = 31 * result + (matchID != null ? matchID.hashCode() : 0);
+        return result;
+    }
 }
